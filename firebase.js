33efc63a440ea449b16ba93aa844752353c6eae9
Hamza Yaafar <107732777+hamzayaafar@users.noreply.github.com>
2023-11-21 13:15:39 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, set, onValue, push } from 'https://www.gstatic.com/fi
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDoStxdho48gIIs4TySsiE-To-VQ02M9tM',
-  authDomain: 'hw10-2b8bc.firebaseapp.com',
-  databaseURL: 'https://hw10-2b8bc-default-rtdb.firebaseio.com',
-  projectId: 'hw10-2b8bc',
-  storageBucket: 'hw10-2b8bc.appspot.com',
-  messagingSenderId: '659036315729',
-  appId: '1:659036315729:web:ab5c221c07d31bdf379870',
-  measurementId: 'G-T8QXSHGPFG'
+  apiKey: '******************',
+  authDomain: '******************',
+  databaseURL: '******************',
+  projectId: '******************',
+  storageBucket: '******************',
+  messagingSenderId: '******************',
+  appId: '*************',
+  measurementId: '******************'
 }
 
 const app = initializeApp(firebaseConfig)
